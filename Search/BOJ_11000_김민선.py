0da7d2c4aa301097ff6c4f94fa60581fe72e9426
minsun <meeeeensun@gmail.com>
2021-03-01 18:01:26 +0900

    2021.03.01 STD

---
+++ Search/BOJ_11000_김민선.py
@@ -0,0 +1,38 @@
+# 강의실 배정
+import heapq
+import sys
+
+N=int(sys.stdin.readline())
+
+time=list()
+standard=list()
+
+for _ in range(N):
+    S,T=map(int,sys.stdin.readline().split())
+    time.append((S,T))
+
+time.sort()
+
+for i in range(N):
+    if len(standard) != 0 and standard[0]<= time[i][0]:
+        heapq.heappop(standard)
+    heapq.heappush(standard,time[i][1])
+    
+print(len(standard))
+
+#import sys
+
+# N = int(sys.stdin.readline())
+# time = []
+# res = []
+# for i in range(N) :
+#     S,T = map(int,sys.stdin.readline().split())
+#     time.append([S,T])
+# time.sort()
+
+# for i in range(N) :
+#     if len(res) != 0 and res[0] <= time[i][0] :
+#         res.remove(min(res))
+#     res.append(time[i][1])
+
+# print(len(res))
---
+++ Search/BOJ_1931_김민선.py
@@ -0,0 +1,20 @@
+#회의실 배정
+import sys
+
+N = int(sys.stdin.readline())
+time = []
+end_time = 0; cnt = 0
+for i in range(N) :
+    a, b = map(int,sys.stdin.readline().split())
+    time.append([a,b])
+
+time = sorted(time, key=lambda a : a[0])
+time = sorted(time, key=lambda a : a[1])
+
+for i, j in time :
+    print("{} {}".format(i, end_time))
+    if i >= end_time :
+        cnt += 1
+        end_time = j
+
+print(cnt)
